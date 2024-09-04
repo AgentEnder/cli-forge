549a316e89fa1c38eae63322bf387ddc2ab192ee
Craigory Coppola <craigorycoppola@gmail.com>
2024-09-03 22:55:45 -0400

    chore(docs-site): update logo

--- docs-site/docusaurus.config.ts
+++ docs-site/docusaurus.config.ts
@@ -135,7 +135,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} CLI Forge. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
